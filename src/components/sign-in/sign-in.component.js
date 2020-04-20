339742499780724a9f1e66f3fcff8e72d73965ec
tanzorr <alexx1984@ukr.net>
2020-04-20 17:39:25 +0300

    sign in with email and password

--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -15,8 +15,16 @@ class SignInComponent extends Component {
 
     }
 
-    handleSubmit = event=>{
+    handleSubmit =async event=>{
         event.preventDefault()
+
+        const {email, password} = this.state
+        try {
+            await auth.signInWithEmailAndPassword(email, password)
+            this.setState({email:'', password:''});
+        }catch (error) {
+            console.log(error)
+        }
 
         this.setState({email:'', password:''})
     }
