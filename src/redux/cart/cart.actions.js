661221c45891fa322a87fc8534995a557aaf1981
tanzorr <alexx1984@ukr.net>
2020-04-23 18:17:23 +0300

    rmove and increase and decrease function is addet

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,20 @@ import CartActionTypes from "./cart.types";
 export const toggleCartHidden = ()=>({
     type:CartActionTypes.TOGGLE_CART_HIDDEN
 })
+
+export const clearItemFromCart = (item)=>{
+    return {
+        type:CartActionTypes.CLEAR_ITEM_FROM_CART,
+        payload:item
+     }
+}
+
+export const addItem = item => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+});
+
+export const removeItem = item=>({
+    type:CartActionTypes.REMOVE_ITEM,
+    payload:item
+})
