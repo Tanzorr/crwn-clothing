c9a01382ee15a815bddd8286168ae76194694c07
tanzorr <alexx1984@ukr.net>
2020-04-25 18:59:42 +0300

    adder stripe button and rying change favicon

--- src/pages/checkout/checkout.component.js
+++ src/pages/checkout/checkout.component.js
@@ -4,6 +4,7 @@ import {createStructuredSelector} from "reselect";
 import {selectCartItems, selectCartTotal} from "../../redux/cart/cart.selectors";
 import './checkout.styles.scss'
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 const CheckoutPage = ({cartItems,total})=>(
     <div className='checkout-page'>
@@ -34,7 +35,14 @@ const CheckoutPage = ({cartItems,total})=>(
         }
         <div className="total">
             <span>TOTAL: ${total}</span>
+            <div className="test-warning">
+                *Please ue the following test credit cart for payments*
+                <br/>
+                4242 4242 4242 4242 - Exp: 01/20 -CVV:123
+            </div>
+            <StripeCheckoutButton price={total}/>
         </div>
+
     </div>
 )
 
